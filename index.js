9f4d87f1cb4aa0fd47fdb6c4d9f4498761d58aa1
Zohaib <zohaibv1999@gmail.com>
2025-08-10 12:25:09 -0400

    removed cors

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const cors  = require('cors')
+
 
 
 const app = express()
 //app.use(cors())
 app.use(express.json())
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
 app.use(express.static('dist'))
 let notes = [
     {
